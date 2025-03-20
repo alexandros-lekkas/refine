a9b946a515c8fe72af4a05ffb5180d45e8bbbe0f
Stelios <stelios.papapan@gmail.com>
2025-03-20 19:09:53 -0400

    feat: Ds

--- src/components/types.ts
+++
@@ -1,5 +0,0 @@
-export type TaskPreviewProps = {};
-export type AIAssistantPreviewProps = {};
-export type ComparisonSectionProps = {};
-export type TestimonialsProps = {};
-export type FeatureGridProps = {};
